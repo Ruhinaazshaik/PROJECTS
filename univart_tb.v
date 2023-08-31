ffa5726c44d8fb16468b1f5688c147d203e4a45c
Ruhinaazshaik <143697811+Ruhinaazshaik@users.noreply.github.com>
2023-08-31 19:43:05 +0530

    Add files via upload
    UART(Universal asynchronous receiver transmitter)
    verilog code
    testbench
    output waveforms
    rtl design

---
+++ univart_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.08.2023 22:04:16
+// Design Name: 
+// Module Name: univart_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module univart_tb(
+
+    );
+endmodule
